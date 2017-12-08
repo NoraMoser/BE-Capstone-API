0580f2aec1f9086fede101c213721e7ca92e72ea
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-12-08 09:40:38 -0600

    gem problems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
   skip_before_action :authenticate_request 
+  before_action :set_user, only: [:show, :update, :destroy]
   
   def authenticate 
     command = AuthenticateUser.call(params[:email], params[:password]) 
