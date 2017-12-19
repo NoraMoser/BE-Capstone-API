adbd6d9c2fb294c363e7863ded25913c2f689087
Nora Moser <noramoser@Noras-MacBook-Pro.local>
2017-12-19 09:54:40 -0600

    added to-buy column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206224526) do
+ActiveRecord::Schema.define(version: 20171219155049) do
 
   create_table "answers", force: :cascade do |t|
     t.string "drop_down_items"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20171206224526) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "to_buy"
     t.index ["user_id"], name: "index_user_books_on_user_id"
   end
 
